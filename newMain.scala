72273ba7d0e6da8c8cd7a2d8b86dd401e1e88956
sakurai <sakurai@em1-112-239-98.pool.e-mobile.ne.jp>
2011-06-26 14:40:22 +0900

    commit23

--- newMain.scala
+++ newMain.scala
@@ -9,7 +9,18 @@ object newMain {
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
-    println("Hello, world 23!")
+    println("Hello, world "+exp(1,"+",(2,"*",3)))
+  }
+
+  def exp(a:Any):Int = {
+    a match {
+    case a:Int => a
+    case (a,"+",b) => exp(a)+exp(b)
+    case (a,"-",b) => exp(a)-exp(b)
+    case (a,"*",b) => exp(a)*exp(b)
+    case (a,"/",b) => exp(a)/exp(b)
+    case _ => throw new Exception("error")
+    }
   }
 
 }
