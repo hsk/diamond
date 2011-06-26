1ea542e8257f64d461423b295c543a5b55a3911a
sakurai <sakurai@em1-112-239-98.pool.e-mobile.ne.jp>
2011-06-26 14:01:48 +0900

    commit2

---
+++ newMain.scala
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+object newMain {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
